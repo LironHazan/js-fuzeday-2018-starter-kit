1150bc377c4a59e2f3223dbdacdfb7dc9e0e58fc
giladglasberg <giladg@tikalk.com>
2018-11-27 13:34:57 +0200

    store for products/cart

--- src/store.js
+++ src/store.js
@@ -4,13 +4,19 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    state: {
+        inCart: ['foo']
+    },
+    mutations: {
+        setCartItems: (state, items) => {
+            state.inCart = items;
+            return state;
+        }
+    },
+    getters: {
+        inCart: (state) => state.inCart
+    },
+    actions: {
+        setCartItems: (context, items) => context.commit('setCartItems', items)
+    }
 })

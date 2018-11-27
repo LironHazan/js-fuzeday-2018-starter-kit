528a5a9d37c0a1e462b5976d2941ba4408b51e66
domainio <takeover991>
2018-11-27 11:03:15 +0200

    search init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
 	<div id="app">
 		<Navbar/>
+		<SearchInput />
 		<router-view/>
 	</div>
 </template>
 <script>
 	import Navbar from "./components/Navbar.vue";
+    import SearchInput from "./components/SearchInput.vue";
 
-	export default {
+
+    export default {
 		components: {
-			Navbar
+			Navbar,
+          SearchInput
 		}
 	};
 </script>

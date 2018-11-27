4a4e8351f2307cd7345ff62fcb8ca92812c6348e
Eli Hen <elihen@tikalk.com>
2018-11-27 15:26:47 +0200

    Add to cart functionality.

--- src/store.js
+++ src/store.js
@@ -8,16 +8,31 @@ export default new Vuex.Store({
         inCart: []
     },
     mutations: {
-        setCartItems: (state, items) => {
-            state.inCart = items;
+        addToCart: (state, item) => {
+            // get stored item in cart
+            let storedItemIndex = state.inCart.findIndex(c => c.itemId === item.itemId);
+            
+            // update or add item to cart
+            if (storedItemIndex !== -1) {
+                state.inCart[storedItemIndex].quantity += item.quantity;
+            } else {
+                state.inCart.push(item);
+                storedItemIndex = state.inCart.length - 1;
+            }
+
+            // remove item from cart if quantity is not greater than 0
+            if (state.inCart[storedItemIndex].quantity <= 0) {
+                state.inCart.splice(storedItemIndex, 1);
+            }
+            
             return state;
-        }
+        },
     },
     getters: {
         inCart: (state) => state.inCart,
-        inCartAmount: (state) => state.inCart.length
+        inCartAmount: (state) => state.inCart.length,
     },
     actions: {
-        setCartItems: (context, items) => context.commit('setCartItems', items)
+        addToCart: (context, item) => context.commit('addToCart', item),
     }
 })

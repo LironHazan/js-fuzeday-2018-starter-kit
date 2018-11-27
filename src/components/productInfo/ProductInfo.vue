aa614a1eab5fac7cf1117580417153ad8bd4820e
domainio <takeover991>
2018-11-27 16:46:51 +0200

    product info

--- src/components/productInfo/ProductInfo.vue
+++ src/components/productInfo/ProductInfo.vue
@@ -1,10 +1,40 @@
 <template>
-    
+    <v-card color="cyan darken-2" class="white--text">
+        <v-layout>
+            <v-flex xs5>
+                <v-img
+                        src={{product.images[0].src}}
+                        height="125px"
+                        contain
+                ></v-img>
+            </v-flex>
+            <v-flex xs7>
+                <v-card-title primary-title>
+                    <div>
+                        <div class="headline">{{product.title}}</div>
+                        <div>{{product.description}}</div>
+                        <div>{{product.vendor}})</div>
+                    </div>
+                </v-card-title>
+            </v-flex>
+        </v-layout>
+        <v-divider light></v-divider>
+        <v-card-actions class="pa-3">
+            Rate this product
+            <v-spacer></v-spacer>
+            <v-icon>star_border</v-icon>
+            <v-icon>star_border</v-icon>
+            <v-icon>star_border</v-icon>
+            <v-icon>star_border</v-icon>
+            <v-icon>star_border</v-icon>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <script>
     export default {
-        name: "ProductInfo"
+        name: "ProductInfo",
+      props: ['product']
     }
 </script>
 

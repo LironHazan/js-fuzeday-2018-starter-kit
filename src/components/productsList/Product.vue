bf40f3200a6d88efc9da1871c0d010d899818584
giladglasberg <giladg@tikalk.com>
2018-11-27 10:54:28 +0200

    initial structure

---
+++ src/components/productsList/Product.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="about">
+		<router-link to="/product">product</router-link>
+		<div>{{product.id}}</div>
+	</div>
+</template>
+<script>
+    export default {
+        name: "Product",
+        props: {
+            product: Object
+        },
+    }
+</script>
---
+++ src/views/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="about">
+		<Cart> </Cart>
+	</div>
+</template>
+<script>
+    // @ is an alias to /src
+    import Cart from '@/components/cart/Cart.vue'
+
+    export default {
+        name: 'cart',
+        components: {
+            Cart
+        }
+    }
+</script>
